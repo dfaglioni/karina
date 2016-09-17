b6e242d4705a8a46d51383a42fd7156c3408379d
eduardo.gardin@elotech.com.br <eduardo.gardin@elotech.com.br>
2016-09-17 11:45:39 -0300

    Projeto inicial

---
+++ src/main/java/br/com/elotech/karina/KarinaApplication.java
@@ -0,0 +1,12 @@
+package br.com.elotech.karina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarinaApplication.class, args);
+	}
+}
