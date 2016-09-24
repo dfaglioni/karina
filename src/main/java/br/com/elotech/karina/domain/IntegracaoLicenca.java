eea7ee9f4bb33eba45208883517855bbcadc4a58
eduardo.gardin@elotech.com.br <eduardo.gardin@elotech.com.br>
2016-09-24 12:41:35 -0300

    Mapeamento das colunas da classe de domínio

--- src/main/java/br/com/elotech/karina/domain/IntegracaoLicenca.java
+++ src/main/java/br/com/elotech/karina/domain/IntegracaoLicenca.java
@@ -2,6 +2,7 @@ package br.com.elotech.karina.domain;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -25,40 +26,72 @@ public class IntegracaoLicenca {
     /**
      * Campos não utilizados
      */
+    @Column(name = "usu_horger")
     private Integer horaGeracaoRepositorio;
 
+    @Column(name = "usu_numctr")
     private Integer numeroInternoContrato;
 
+    @Column(name = "usu_seqcvs")
     private Integer sequenciaItemServico;
+
+    @Column(name = "usu_codcli")
     private Integer codigoClienteContrato;
 
+    @Column(name = "usu_nomcli")
     private String nomeCliente;
+
+    @Column(name = "usu_codser")
     private String codigoServico;
 
+    @Column(name = "usu_sitctr")
     private Situacao situacaoContrato;
+
+    @Column(name = "usu_sitcvs")
     private Situacao situacaoServico;
 
+    @Column(name = "usu_codrep")
     private Long codigoRepresentante;
+
+    @Column(name = "usu_numtit")
     private String numeroTituloMovimentado;
+
+    @Column(name = "usu_codtpt")
     private String tipoTitulo;
 
+    @Column(name = "usu_seqmov")
     private Integer sequenciaMovimentoTitulo;
 
+    @Column(name = "usu_emitit")
     private LocalDate emissaoTitulo;
+
+    @Column(name = "usu_vcttit")
     private LocalDate vencimentoTitulo;
+
+    @Column(name = "usu_baitit")
     private LocalDate quitacaoTitulo;
+
+    @Column(name = "usu_cptctr")
     private LocalDate competenciaServicoContrato;
+
+    @Column(name = "usu_datger")
     private LocalDate dataGeracaoRepositorio;
 
+    @Column(name = "usu_usuger")
     private Integer usuarioGeracaoRepositorio;
 
+    @Column(name = "usu_tipger")
     private TipoGeracao tipoGeracao;
+
+    @Column(name = "usu_blolib")
     private TipoRegistro tipoRegistro;
 
+    @Column(name = "usu_codmot")
     private Integer motivoServicoContrato;
-    private String observacao;
-    private TipoAcao tipoAcao;
 
-    // Nossa coluna para dizer se foi processado ou não o registro
-    private Boolean processado;
+    @Column(name = "usu_obsitg")
+    private String observacao;
+
+    @Column(name = "usu_tipaca")
+    private TipoAcao tipoAcao;
 }
--- src/main/java/br/com/elotech/karina/domain/IntegracaoLicencaPk.java
+++ src/main/java/br/com/elotech/karina/domain/IntegracaoLicencaPk.java
@@ -2,7 +2,9 @@ package br.com.elotech.karina.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +22,15 @@ public class IntegracaoLicencaPk implements Serializable {
      */
     private static final long serialVersionUID = -3301626592011538493L;
 
+    @Column(name = "usu_codemp")
+    @NotNull
     private Integer codigoEmpresa;
+
+    @Column(name = "usu_codfil")
+    @NotNull
     private Integer codigoFilial;
+
+    @Column(name = "usu_seqitg")
+    @NotNull
     private Integer sequenciaIntegracao;
 }
