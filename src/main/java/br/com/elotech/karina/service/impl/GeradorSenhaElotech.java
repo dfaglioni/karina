6681b5ca86ad6b5d8380a55b2e18c17eb4063f2d
Douglas Boleta <douglas.boleta@elotech.com.br>
2016-12-17 11:31:08 -0200

    fix senha até idea

--- src/main/java/br/com/elotech/karina/service/impl/GeradorSenhaElotech.java
+++ src/main/java/br/com/elotech/karina/service/impl/GeradorSenhaElotech.java
@@ -1,6 +1,7 @@
 package br.com.elotech.karina.service.impl;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.bouncycastle.crypto.BufferedBlockCipher;
@@ -32,29 +33,32 @@ public class GeradorSenhaElotech implements GeradorSenha {
     @SneakyThrows
     private String internalGenerate(String concatLicense) {
 
-        byte[] key = DigestUtils.getSha1Digest().digest(DigestUtils.md5Hex(KEY_ELOTECH).getBytes());
+        System.out.println(concatLicense);
 
-        KeyParameter keyParameter = new KeyParameter(key);
-        BufferedBlockCipher cipher = new BufferedBlockCipher(new IDEAEngine());
-        cipher.init(true, keyParameter);
+        String keyForSha1 = DigestUtils.md5Hex(KEY_ELOTECH).toUpperCase();
+        System.out.println(keyForSha1);
+
+        String keyForIdea = new String(Hex.encode(DigestUtils.getSha1Digest().digest(keyForSha1.getBytes())))
+                .toUpperCase();
+
+        System.out.println(keyForIdea);
 
         byte[] input = concatLicense.getBytes();
-
         byte[] out = new byte[input.length];
+
+        KeyParameter keyParameter = new KeyParameter(keyForIdea.getBytes());
+
+        BufferedBlockCipher cipher = new BufferedBlockCipher(new IDEAEngine());
+
+        cipher.init(true, keyParameter);
 
         cipher.processBytes(input, 0, input.length, out, 0);
 
-        String ideaSecret = new String(Hex.encode(out)).toUpperCase();
-        StringBuilder licenseKey = new StringBuilder();
+        String ideaSecret = Base64.getEncoder().encodeToString(out);
 
-        for (int i = 0; i < ideaSecret.length(); i++) {
+        System.out.println(ideaSecret);
 
-            if (i % 9 == 0) {
-                licenseKey.append(ideaSecret.charAt(i));
-            }
-        }
-
-        return licenseKey.toString();
+        return null;
 
     }
 
--- src/test/java/br/com/elotech/karina/service/impl/GeradorSenhaMD5Test.java
+++ src/test/java/br/com/elotech/karina/service/impl/GeradorSenhaMD5Test.java
@@ -16,16 +16,6 @@ public class GeradorSenhaMD5Test {
 
     @Test
     public void maringaTributos() {
-        /*
-         * Temp := MD5Hash(KeyString); IDEA_Buffer.InitStr(AnsiString(Temp),
-         * TDCP_sha1); Temp :=
-         * String(IDEA_Buffer.EncryptString(AnsiString(Buffer))); Result :=
-         * GetHexa(Temp);
-         */
-
-        // key '404229D7D58EF5C9DAF4613EC604BE27'
-        // idea
-        // '44346C77484F2B696830784A6562636A66685879696973332F4557323473736A5644496478767A46416550716D7555742B614F58712F32445577755A784A34703234694C4A35733D'
 
         String senha = senhaMD5.generate(
                 License.builder().module(Module.AISE_TRIBUTOS).code("306137001").date(LocalDate.of(2016, 12, 15))
@@ -34,5 +24,23 @@ public class GeradorSenhaMD5Test {
         assertThat(senha, equalTo("486877644D6F744D"));
 
     }
+
+    /*
+     * KeySource '15/12/2016PREFEITURA DO MUNICIPIO DE MARINGA306137001' DELPHI
+     * '15/12/2016PREFEITURA DO MUNICIPIO DE MARINGA306137001' JAVA
+     * 
+     * MD5 404229D7D58EF5C9DAF4613EC604BE27 404229D7D58EF5C9DAF4613EC604BE27
+     * 
+     * SHA1 5B153A2BCDDC7294290729F06AD9A7C71D2D92A3
+     * 5B153A2BCDDC7294290729F06AD9A7C71D2D92A3
+     * 
+     * IDEA
+     * D4lwHO+ih0xJebcjfhXyiis3/EW24ssjVDIdxvzFAePqmuUt+aOXq/2DUwuZxJ4p24iLJ5s=
+     * 
+     * HEX
+     * 44346C77484F2B696830784A6562636A66685879696973332F4557323473736A5644496478767A46416550716D7555742B614F58712F32445577755A784A34703234694C4A35733D
+     * 
+     * 
+     */
 
 }
